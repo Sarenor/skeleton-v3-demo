f5f2d9e39c022ca82082cde1f9b0eb5caaa7d351
Dominik Röschke <dominik.roeschke@web.de>
2024-11-03 12:31:19 +0100

    Initialized Example Repository

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import forms from '@tailwindcss/forms';
+import type { Config } from 'tailwindcss';
+import { join } from 'path';
+import { skeleton } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
+
+export default {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+        join(require.resolve('@skeletonlabs/skeleton-svelte'), '../**/*.{html,js,svelte,ts}')
+	],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [
+		forms,
+		skeleton({
+            // NOTE: each theme included will be added to your CSS bundle
+            themes: [ themes.cerberus, themes.rose ]
+        })
+	]
+} satisfies Config;
